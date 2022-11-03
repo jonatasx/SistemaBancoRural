d1bd07b4a0ee86c268de45c8a4bf521cd5377cac
jonatasx <jonatas.santospessoa@gmail.com>
2022-11-03 11:46:12 -0300

    Encapsulamento e herança OOP

---
+++ Conta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancoRural.Contas
+{
+   public enum TipoConta { ContaCorrente, ContaPoupaca};
+    public class Conta 
+    {
+        public string Num;
+        private double _saldo; //convenção de que é private
+        public TipoConta Tipo;
+
+        public Conta(TipoConta tipo, string num)
+        {
+            this.Num = num;
+            this.Tipo = tipo;
+            _saldo = 0;
+        }
+
+        public void Sacar (double valor)
+        {
+            _saldo = _saldo - valor;
+        }
+        public void Depositar (double valor)
+        {
+            _saldo = _saldo + valor;
+        }
+        public double GetSaldo()
+        {
+            return _saldo;//variável somente leitura
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SistemaBancoRural.Contas;
+
+namespace SistemaBancoRural
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta(TipoConta.ContaCorrente,"123456");
+            Console.WriteLine("Mostra saldo:");
+            conta.Num = "123";
+            
+            conta.Depositar(500);
+            conta.Sacar(300);
+            Console.WriteLine(conta.GetSaldo().ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
